b7497010626d9576fb0a4d8122e86bda29162e8a
phoniccgirish <gy63862829@gmail.com>
2026-08-20 12:51:28 +0530

    new

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-60fd49aa'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.cht7jlramjo"
+    "revision": "0.jonm5ldpdpo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
