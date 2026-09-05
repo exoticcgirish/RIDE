bf12be3c2ca7b39882a43ebe0496ee43d53fe007
phoniccgirish <gy63862829@gmail.com>
2026-09-05 14:44:56 +0530

    change frontend url

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,7 +14,7 @@ const corsOptions = {
   origin: [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://ride-eight-nu.vercel.app",
+    "https://ridelinkgo.vercel.app",
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
